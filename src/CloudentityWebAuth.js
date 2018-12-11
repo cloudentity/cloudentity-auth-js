0a1e71a960499cb5f5d7ac579672e20b977b0578
Bartosz Wereszczak <bwereszczak@syntegrity.com>
2018-12-11 16:21:08 +0100

    Configurable 'authorizationUri' and 'userInfoUri' when this urls needs to by customized. 'domain' option has some predefined values provided e.g. protocol, pathname

--- src/CloudentityWebAuth.js
+++ src/CloudentityWebAuth.js
@@ -1,5 +1,5 @@
 import ClientOAuth2 from 'client-oauth2';
-import {notEmptyStringArray, notEmptyString, validateObject} from "./utils/validators";
+import { notEmptyStringArray, notEmptyString, validateObject } from "./utils/validators";
 import superagent from 'superagent';
 
 const ERRORS = {
@@ -9,13 +9,13 @@ const ERRORS = {
 
 const optionsSpec = {
   clientId: [
-    {test: notEmptyString, message: '\'cliendId\' [non-empty string] option is required'},
+    {test: notEmptyString, message: '\'cliendId\' [non-empty string] option is required'}
   ],
-  domain: [
-    {test: notEmptyString, message: '\'domain\' [non-empty string] option is required'},
-  ],
+  domain: [],
+  authorizationUri: [],
+  userInfoUri: [],
   redirectUri: [
-    {test: notEmptyString, message: '\'redirectUri\' [non-empty string] option is required'},
+    {test: notEmptyString, message: '\'redirectUri\' [non-empty string] option is required'}
   ],
   scopes: [
     {test: notEmptyStringArray, message: '\'scopes\' [non-empty array of strings] option is required'}
@@ -33,7 +33,7 @@ class CloudentityWebAuth {
    *
    * @param {Object} options
    */
-  constructor(options) {
+  constructor (options) {
     this.options = CloudentityWebAuth._parseOptions(options);
 
     const {clientId, redirectUri, authorizationUri, scopes} = this.options;
@@ -49,7 +49,7 @@ class CloudentityWebAuth {
   /**
    * Initiates OAuth2 implicit flow (redirecting to Cloudentity authorization page)
    */
-  authorize() {
+  authorize () {
     global.window.location.href = this.oauth.token.getUri();
   }
 
@@ -60,7 +60,7 @@ class CloudentityWebAuth {
    *
    * @returns {Promise<ClientOAuth2.Token>}
    */
-  getAuth() {
+  getAuth () {
     return this.oauth.token.getToken(global.window.location.href).then(
       auth => auth.tokenType && auth.accessToken && !auth.expired() ? Promise.resolve(auth.data) : Promise.reject({error: ERRORS.UNAUTHORIZED})
     );
@@ -72,22 +72,22 @@ class CloudentityWebAuth {
    *
    * @returns {Promise}
    */
-  userInfo(accessToken) {
+  userInfo (accessToken) {
     return superagent.get(this.options.userInfoUri).set('Authorization', 'Bearer ' + accessToken).then(
       res => res.body,
       rej => Promise.reject(rej.status === 401 ? {error: ERRORS.UNAUTHORIZED} : {error: ERRORS.ERROR, message: rej.response.body})
     );
   }
 
-  static _parseOptions(options) {
+  static _parseOptions (options) {
     const error = validateOptions(options);
 
     if (error) {
       throw new Error(error);
     }
 
-    options.authorizationUri = `https://${options.domain}/oauth/authorize`;
-    options.userInfoUri = `https://${options.domain}/oauth/userinfo`;
+    options.authorizationUri = options.domain ? `https://${options.domain}/oauth/authorize` : options.authorizationUri;
+    options.userInfoUri = options.domain ? `https://${options.domain}/oauth/userinfo` : options.userInfoUri;
 
     return options;
   }
